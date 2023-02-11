e52be4aeb05e4147a23ed25e3d58bdd1e63e9863
罗旭宏 <barntet@163.com>
2023-02-11 18:06:46 +0800

    feat: link-cms

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,10 @@ import App from 'next/app';
 import { Layout, ILayoutProps } from '@/components/layout';
 import code from '@/public/code.png';
 import Head from 'next/head';
+import axios from 'axios';
+import { LOCALDOMAIN } from '@/utils';
+
+console.log(import.meta);
 
 const MyApp = (data: AppProps & ILayoutProps) => {
   const { Component, pageProps, navbarData, footerData } = data;
@@ -24,44 +28,50 @@ const MyApp = (data: AppProps & ILayoutProps) => {
 
 MyApp.getInitialProps = async (context: AppContext) => {
   const pageProps = await App.getInitialProps(context);
+  const { data = {} } = await axios.get(`${LOCALDOMAIN}/api/layout`);
 
   return {
     ...pageProps,
-    navbarData: {},
-    footerData: {
-      title: 'demo',
-      linkList: [
-        {
-          title: '技术栈',
-          list: [
-            { label: 'react' },
-            { label: 'Typescript' },
-            { label: 'ssr' },
-            { label: 'nodejs' },
-          ],
-        },
-        {
-          title: '了解更多',
-          list: [
-            { label: '掘金', link: '' },
-            { label: '知乎', link: '' },
-            { label: 'csdn', link: '' },
-          ],
-        },
-        {
-          title: '联系我',
-          list: [{ label: '微信' }, { label: 'QQ' }],
-        },
-      ],
-      qrCode: {
-        image: code,
-        text: '前端',
-      },
-      copyRight: 'Copyright @ 2022 xxx 保留所有权利',
-      siteNumber: '粤ICP备xxxxxxxx号-x',
-      publicNumber: '粤公网安备 xxxxxxxxxx号',
-    },
+    ...data,
   };
+
+  // return {
+  //   ...pageProps,
+  //   navbarData: {},
+  //   footerData: {
+  //     title: 'demo',
+  //     linkList: [
+  //       {
+  //         title: '技术栈',
+  //         list: [
+  //           { label: 'react' },
+  //           { label: 'Typescript' },
+  //           { label: 'ssr' },
+  //           { label: 'nodejs' },
+  //         ],
+  //       },
+  //       {
+  //         title: '了解更多',
+  //         list: [
+  //           { label: '掘金', link: '' },
+  //           { label: '知乎', link: '' },
+  //           { label: 'csdn', link: '' },
+  //         ],
+  //       },
+  //       {
+  //         title: '联系我',
+  //         list: [{ label: '微信' }, { label: 'QQ' }],
+  //       },
+  //     ],
+  //     qrCode: {
+  //       image: code,
+  //       text: '前端',
+  //     },
+  //     copyRight: 'Copyright @ 2022 xxx 保留所有权利',
+  //     siteNumber: '粤ICP备xxxxxxxx号-x',
+  //     publicNumber: '粤公网安备 xxxxxxxxxx号',
+  //   },
+  // };
 };
 
 export default MyApp;
---
+++ pages/api/layout.ts
@@ -0,0 +1,49 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import axios from "axios";
+import { ILayoutProps } from "../../components/layout";
+import { CMSDOMAIN, LOCALDOMAIN } from "@/utils";
+import { isEmpty } from "lodash";
+
+const getLayoutData = (
+  req: NextApiRequest,
+  res: NextApiResponse<ILayoutProps>
+) => {
+  axios.get(`${CMSDOMAIN}/api/layouts`).then((result) => {
+    const {
+      copyRight,
+      links,
+      publicNumber,
+      qr_code,
+      qr_code_image,
+      siteNumber,
+      title,
+    } = result.data || {};
+
+    res.status(200).json({
+      navbarData: {},
+      footerData: {
+        title,
+        linkList: links?.data?.map((item: any) => {
+          return {
+            title: item.title,
+            list: item?.links?.data?.map((_item: any) => {
+              return {
+                label: _item.label,
+                link: isEmpty(_item.link) ? "" : _item.link,
+              };
+            }),
+          };
+        }),
+        qrCode: {
+          image: `${LOCALDOMAIN}/public/code}`,
+          text: qr_code,
+        },
+        copyRight,
+        siteNumber,
+        publicNumber,
+      },
+    });
+  });
+};
+
+export default getLayoutData;
